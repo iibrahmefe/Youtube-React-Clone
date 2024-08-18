b1c1fa85c5115cf7f6fb7f61ff0e00fe497db117
MedellinCodes <118635082+MedellinCodes@users.noreply.github.com>
2024-08-18 03:42:28 +0300

    geçmiş sayfası revize

--- src/pages/history/index.jsx
+++ src/pages/history/index.jsx
@@ -74,91 +74,109 @@ export default function History() {
                                 </div>
                                 <hr className="mt-10" />
                                 <div className="w-full flex flex-col gap-2 mt-4 ">
-                                    <div className="flex  gap-4 px-4 ">
+                                    <div className="flex 2xl:flex-row flex-col gap-4 px-4 ">
                                         <div className=" object-cover overflow-hidden rounded-md relative ">
                                             <span className="absolute h-1 w-3/4 bg-red-600 bottom-0 right-0 left-0"></span>
                                             <span className="absolute px-1 py-px bottom-1  right-1 bg-black/70 rounded-md text-sm">20:40</span>
-                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" />
+                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" className="block w-full" />
                                         </div>
                                         <div className="flex-1  justify-between flex">
                                             <div className="flex flex-col gap-2 ">
                                                 <h2 className="font-semibold text-wrap whitespace-nowrap leading-4 overflow-hidden ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, similique cum </h2>
                                                 <p className="text-white/30 text-sm">Kanal ismi . <span>9B görüntülenme</span></p>
-                                                <p className="text-white/30 text-sm mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
+                                                <p className="text-white/30 text-sm mt-1 line-clamp-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
                                             </div>
                                             <div className=" text-2xl text-white/50 ">
                                                 <IoMdClose />
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="flex  gap-4 px-4 ">
+                                    <div className="flex 2xl:flex-row flex-col gap-4 px-4 ">
                                         <div className=" object-cover overflow-hidden rounded-md relative ">
                                             <span className="absolute h-1 w-3/4 bg-red-600 bottom-0 right-0 left-0"></span>
                                             <span className="absolute px-1 py-px bottom-1  right-1 bg-black/70 rounded-md text-sm">20:40</span>
-                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" />
+                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" className="block w-full" />
                                         </div>
                                         <div className="flex-1  justify-between flex">
                                             <div className="flex flex-col gap-2 ">
                                                 <h2 className="font-semibold text-wrap whitespace-nowrap leading-4 overflow-hidden ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, similique cum </h2>
                                                 <p className="text-white/30 text-sm">Kanal ismi . <span>9B görüntülenme</span></p>
-                                                <p className="text-white/30 text-sm mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
+                                                <p className="text-white/30 text-sm mt-1 line-clamp-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
                                             </div>
                                             <div className=" text-2xl text-white/50 ">
                                                 <IoMdClose />
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="flex  gap-4 px-4 ">
+                                    <div className="flex 2xl:flex-row flex-col gap-4 px-4 ">
                                         <div className=" object-cover overflow-hidden rounded-md relative ">
                                             <span className="absolute h-1 w-3/4 bg-red-600 bottom-0 right-0 left-0"></span>
                                             <span className="absolute px-1 py-px bottom-1  right-1 bg-black/70 rounded-md text-sm">20:40</span>
-                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" />
+                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" className="block w-full" />
                                         </div>
                                         <div className="flex-1  justify-between flex">
                                             <div className="flex flex-col gap-2 ">
                                                 <h2 className="font-semibold text-wrap whitespace-nowrap leading-4 overflow-hidden ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, similique cum </h2>
                                                 <p className="text-white/30 text-sm">Kanal ismi . <span>9B görüntülenme</span></p>
-                                                <p className="text-white/30 text-sm mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
+                                                <p className="text-white/30 text-sm mt-1 line-clamp-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
                                             </div>
                                             <div className=" text-2xl text-white/50 ">
                                                 <IoMdClose />
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="flex  gap-4 px-4 ">
+                                    <div className="flex 2xl:flex-row flex-col gap-4 px-4 ">
                                         <div className=" object-cover overflow-hidden rounded-md relative ">
                                             <span className="absolute h-1 w-3/4 bg-red-600 bottom-0 right-0 left-0"></span>
                                             <span className="absolute px-1 py-px bottom-1  right-1 bg-black/70 rounded-md text-sm">20:40</span>
-                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" />
+                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" className="block w-full" />
                                         </div>
                                         <div className="flex-1  justify-between flex">
                                             <div className="flex flex-col gap-2 ">
                                                 <h2 className="font-semibold text-wrap whitespace-nowrap leading-4 overflow-hidden ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, similique cum </h2>
                                                 <p className="text-white/30 text-sm">Kanal ismi . <span>9B görüntülenme</span></p>
-                                                <p className="text-white/30 text-sm mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
+                                                <p className="text-white/30 text-sm mt-1 line-clamp-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
                                             </div>
                                             <div className=" text-2xl text-white/50 ">
                                                 <IoMdClose />
                                             </div>
                                         </div>
                                     </div>
-                                    <div className="flex  gap-4 px-4 ">
+                                    <div className="flex 2xl:flex-row flex-col gap-4 px-4 ">
                                         <div className=" object-cover overflow-hidden rounded-md relative ">
                                             <span className="absolute h-1 w-3/4 bg-red-600 bottom-0 right-0 left-0"></span>
                                             <span className="absolute px-1 py-px bottom-1  right-1 bg-black/70 rounded-md text-sm">20:40</span>
-                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" />
+                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" className="block w-full" />
                                         </div>
                                         <div className="flex-1  justify-between flex">
                                             <div className="flex flex-col gap-2 ">
                                                 <h2 className="font-semibold text-wrap whitespace-nowrap leading-4 overflow-hidden ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, similique cum </h2>
                                                 <p className="text-white/30 text-sm">Kanal ismi . <span>9B görüntülenme</span></p>
-                                                <p className="text-white/30 text-sm mt-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
+                                                <p className="text-white/30 text-sm mt-1 line-clamp-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
                                             </div>
                                             <div className=" text-2xl text-white/50 ">
                                                 <IoMdClose />
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="flex 2xl:flex-row flex-col gap-4 px-4 ">
+                                        <div className=" object-cover overflow-hidden rounded-md relative ">
+                                            <span className="absolute h-1 w-3/4 bg-red-600 bottom-0 right-0 left-0"></span>
+                                            <span className="absolute px-1 py-px bottom-1  right-1 bg-black/70 rounded-md text-sm">20:40</span>
+                                            <img src="https://i.ytimg.com/vi/pUYxthJ1BjU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCgzY-mfS-_uBL-iGhHjpifFfgNuQ" className="block w-full" />
+                                        </div>
+                                        <div className="flex-1  justify-between flex">
+                                            <div className="flex flex-col gap-2 ">
+                                                <h2 className="font-semibold text-wrap whitespace-nowrap leading-4 overflow-hidden ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, similique cum </h2>
+                                                <p className="text-white/30 text-sm">Kanal ismi . <span>9B görüntülenme</span></p>
+                                                <p className="text-white/30 text-sm mt-1 line-clamp-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum libero at quidem nesciunt aut, beatae praesentium commodi eos accusantium iste exercitationem esse adipisci nemo. Molestias possimus quam facere ut dolore!</p>
+                                            </div>
+                                            <div className=" text-2xl text-white/50 ">
+                                                <IoMdClose />
+                                            </div>
+                                        </div>
+                                    </div>
+                                   
                                 </div>
                             </div>
                         </div>
